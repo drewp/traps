917bda0bd0c247577b3c10c78dd810c1e23505df
drewp@bigasterisk.com <drewp@bigasterisk.com>
2020-04-08 00:17:10 -0700

    move globals into a class at least-- trying to still keep this beginner-friendly. HTH

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ![screenshot](game1.png)
 
-Run around with arrow keys, setting traps with spacebar. They blend in with the scenery but they'll still trap you or the enemy.
+Run around with arrow keys, setting traps with spacebar. They blend in with the scenery, but they'll still trap you or the enemy.
 The more you win, the faster the enemy gets.
 
 Depends on urwid (python3-urwid on debian/ubuntu; urwid on PyPI)
--- traps.py
+++ traps.py
@@ -3,88 +3,85 @@ import random
 import math
 import urwid
 
-width, height = 20, 20
 
-x, y = 10, 10
-ex, ey = 2, 3
-eWins = 0
-playerWins = 0
-lastEnemyStep = 0
-boardSetup = 0
-runAwaySteps = 0
-traps = [
-    # x, y, placetime
-]
+class Game:
+    width, height = 20, 20
+
+    x, y = 10, 10
+    ex, ey = 2, 3
+    eWins = 0
+    playerWins = 0
+    lastEnemyStep = 0
+    boardSetup = 0
+    runAwaySteps = 0
+    traps = [
+        # x, y, placetime
+    ]
 
 
 def chase():
-    global ex, ey, lastEnemyStep
     now = time.time()
-    ePeriod = max(.1, 1 - playerWins * .2)
-    if now < lastEnemyStep + ePeriod:
+    ePeriod = max(.1, 1 - Game.playerWins * .2)
+    if now < Game.lastEnemyStep + ePeriod:
         return
-    lastEnemyStep = now
+    Game.lastEnemyStep = now
 
     dx = random.choice([-1, 0, 1])
     dy = random.choice([-1, 0, 1])
     if random.random() < .5:
-        dx = 1 if x > ex else -1
-        dy = 1 if y > ey else -1
+        dx = 1 if Game.x > Game.ex else -1
+        dy = 1 if Game.y > Game.ey else -1
 
-    ex = ex + dx
-    ey = ey + dy
+    Game.ex = Game.ex + dx
+    Game.ey = Game.ey + dy
 
 
 def trapEnemy():
-    global playerWins
-    playerWins = playerWins + 1
+    Game.playerWins = Game.playerWins + 1
     resetBoard("Dog fell in a trap")
 
 
 def enemyHits():
-    global eWins
-    eWins = eWins + 1
+    Game.eWins = Game.eWins + 1
     resetBoard("Dog catches you")
 
 
 def trapPlayer():
-    global eWins
-    eWins = eWins + 1
+    Game.eWins = Game.eWins + 1
     resetBoard("You fell in a trap")
 
 
 def resetBoard(message):
-    global boardSetup, x, y, ex, ey
     footer.set_text(message)
-    boardSetup = 0
-    x = random.randrange(width)
-    y = random.randrange(height // 2)
-    ex = random.randrange(width)
-    ey = random.randrange(height // 2 - 5) + height // 2 + 5
-    traps[:] = []
+    Game.boardSetup = 0
+    Game.x = random.randrange(Game.width)
+    Game.y = random.randrange(Game.height // 2)
+    Game.ex = random.randrange(Game.width)
+    Game.ey = random.randrange(Game.height // 2 - 5) + Game.height // 2 + 5
+    Game.traps[:] = []
 
 
 def stepAway():
-    global x, y
     allowed = []
     for dx in [-1, 0, 1]:
         for dy in [-1, 0, 1]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < width and 0 <= ny < height and (nx, ny) != (
-                    x, y) and (nx, ny) != (ex, ey):
+            nx, ny = Game.x + dx, Game.y + dy
+            if 0 <= nx < Game.width and 0 <= ny < Game.height and (nx, ny) != (
+                    Game.x, Game.y) and (nx, ny) != (Game.ex, Game.ey):
                 allowed.append((nx, ny))
 
     random.shuffle(allowed)
     for nx, ny in allowed:
         if getTrap(nx, ny) is None:
-            x, y = nx, ny
+            Game.x, Game.y = nx, ny
             return
 
-    # they all have traps- you're stuck, so just sit on the trap you placed and lose
+    # they all have traps- you're stuck, so just sit on the trap you placed
+    # and lose
 
 
 def getTrap(qx, qy):
-    for tx, ty, placed in traps:
+    for tx, ty, placed in Game.traps:
         if (tx, ty) == (qx, qy):
             return placed
     return None
@@ -94,22 +91,23 @@ def draw():
     now = time.time()
     out = ""
     rnd = random.Random(36)
-    if (x, y) == (ex, ey):
+    if (Game.x, Game.y) == (Game.ex, Game.ey):
         enemyHits()
-    for by in range(height):
+    for by in range(Game.height):
         outRow = ""
-        for bx in range(width):
+        for bx in range(Game.width):
             cell = rnd.choice("abcdefghijklmnopqrsuvwxyz") + ' '
-            if boardSetup is not None:
-                dist = math.hypot(bx - width / 2, by - height / 2) / width / 2
-                if dist > boardSetup:
+            if Game.boardSetup is not None:
+                dist = math.hypot(bx - Game.width / 2,
+                                  by - Game.height / 2) / Game.width / 2
+                if dist > Game.boardSetup:
                     cell = '  '
             trap = getTrap(bx, by)
-            if (bx, by) == (x, y):
+            if (bx, by) == (Game.x, Game.y):
                 cell = "😲"
                 if trap is not None:
                     trapPlayer()
-            if (bx, by) == (ex, ey):
+            if (bx, by) == (Game.ex, Game.ey):
                 cell = "🐕"
                 if trap is not None:
                     trapEnemy()
@@ -127,34 +125,32 @@ def draw():
 
 
 def update(loop, _):
-    global boardSetup, runAwaySteps
     chase()
     txt.set_text(draw())
-    if runAwaySteps > 0:
+    if Game.runAwaySteps > 0:
         stepAway()
-        runAwaySteps = runAwaySteps - 1
-    if boardSetup is not None:
-        boardSetup = boardSetup + .03
-        if boardSetup >= 1:
-            boardSetup = None
-            footer.set_text(f'dog: {eWins}   you: {playerWins}')
+        Game.runAwaySteps = Game.runAwaySteps - 1
+    if Game.boardSetup is not None:
+        Game.boardSetup = Game.boardSetup + .03
+        if Game.boardSetup >= 1:
+            Game.boardSetup = None
+            footer.set_text(f'dog: {Game.eWins}   you: {Game.playerWins}')
 
     loop.set_alarm_in(.05, update)
 
 
 def onKey(key):
-    global x, y
     if key == 'left':
-        x = max(x - 1, 0)
+        Game.x = max(Game.x - 1, 0)
     if key == 'right':
-        x = min(x + 1, width - 1)
+        Game.x = min(Game.x + 1, Game.width - 1)
     if key == 'up':
-        y = max(y - 1, 0)
+        Game.y = max(Game.y - 1, 0)
     if key == 'down':
-        y = min(y + 1, height - 1)
+        Game.y = min(Game.y + 1, Game.height - 1)
     if key == ' ':
-        traps.append((x, y, time.time()))
-        runAwaySteps = 3
+        Game.traps.append((Game.x, Game.y, time.time()))
+        Game.runAwaySteps = 3
         stepAway()
 
 
